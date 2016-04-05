dfe49b328b89bcfa9cb676b7bc7500a24f1a4172
RIJO JOY <rijojoy@gmail.com>
2016-04-05 07:17:34 +0530

    First commit

---
+++ js/script.js
@@ -0,0 +1,41 @@
+ 
+ $(function() {
+	var tableBody;
+	tableBody = $( "#create-table-html" ).html();
+   
+	
+	$( "#create-table" ).on("click", function(e){
+		e.preventDefault();
+		createTable();
+	});
+    
+	$(document).on( "click",".edit_table_click", function(e) {
+		
+		$(this).closest('tr').next().next('.edit-table').toggle();
+	});
+	$(document).on( "click",".save-edit-table", function(e) {
+		var table_name = $(this).prevAll('input').val();
+		var table_comment = $(this).prevAll('textarea').val();
+		var title_cmt_obj = $(this).parent()   // Finds the closest row <tr> 
+                     .parent()     // Gets a descendent with class="nr"
+                     .parent()
+					 .parent();
+					
+		title_cmt_obj.find(".table_name").html(table_name); // Append table name 
+		if( table_comment != '' )
+		{
+			title_cmt_obj.find(".table-comment").html(table_comment); // Append table comment
+			title_cmt_obj.find(".comment").show(); // Show the table comment 
+		}
+		
+		
+	});
+	function createTable()
+	{
+		$( "#graph" ).after(tableBody);
+		$( ".entity-table" ).draggable({
+				cursor: 'move',
+		 });
+	}
+  
+  });
